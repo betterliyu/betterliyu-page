22401c177b1e9c1bff85e9ff9f4934af66b34e96
betterliyu <liyu@betterliyu.site>
2019-12-11 11:15:34 +0800

    change .deploy.sh

--- .deploy.sh
+++ .deploy.sh
@@ -1,22 +1,23 @@
-  #!/bin/bash 
-  git config --global user.email "liyu@betterliyu.site"
-  git config --global user.name "betterliyu" 
-  mkdir ./publish 
-  cd ./publish
-  git init 
-  git remote add origin https://betterliyu:$1@github.com/betterliyu/betterliyu.github.io.git
-  git fetch origin
-  git checkout -b master origin/master
-  cd ..
-  find ./publish/ | grep -v .git | grep -v . | grep -v .. | xargs rm -rf
-  echo "copy dist to publish..." 
-  cp -r ./dist/. ./publish
-  cd ./publish
-  if [ -z "$(git status --porcelain)" ] then
-      echo "nothing to update."
-  else
-      git add .
-      git commit -m "Update `date -u +'%Y-%m-%dT%H:%M:%SZ'`" -a
-      git push origin master -f
-  fi
-  exit 0
+#!/bin/bash 
+git config --global user.email "liyu@betterliyu.site"
+git config --global user.name "betterliyu" 
+mkdir ./publish 
+cd ./publish
+git init 
+git remote add origin https://betterliyu:$1@github.com/betterliyu/betterliyu.github.io.git
+git fetch origin
+git checkout -b master origin/master
+cd ..
+find ./publish/ | grep -v .git | grep -v . | grep -v .. | xargs rm -rf
+echo "copy dist to publish..." 
+cp -r ./dist/. ./publish
+cd ./publish
+git status
+if [ -z "$(git status --porcelain)" ] then
+    echo "nothing to update."
+else
+    git add .
+    git commit -m "Update `date -u +'%Y-%m-%dT%H:%M:%S.%3NZ'`" -a
+    git push origin master -f
+fi
+exit 0
